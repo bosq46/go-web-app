4938e0ae9e24fd97482311f7896fed191f683a41
mori <khk.mori@gmail.com>
2021-09-26 16:58:36 +0900

    add form sample

--- gin-app.go
+++ gin-app.go
@@ -1,16 +1,24 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
-    router.LoadHTMLGlob("templates/*.html")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
 
-    router.GET("/", func(ctx *gin.Context){
-        ctx.HTML(200, "index.html", gin.H{})
-    })
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{})
+	})
 
-    router.Run()
+	router.GET("/xss", func(ctx *gin.Context) {
+		ctx.HTML(200, "xss.html", gin.H{})
+	})
+	router.POST("/xss", func(ctx *gin.Context) {
+		data := ctx.PostForm("data")
+		// そのまま表示してもデフォルトでHTMLタグはサニタイズされた
+		ctx.HTML(200, "xss.html", gin.H{"data": data})
+	})
+	router.Run()
 }

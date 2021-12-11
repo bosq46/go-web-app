42af7ae54673a5130a771c977151ae6362864fed
mori <khk.mori@gmail.com>
2021-12-11 15:32:01 +0900

    path parameterを試した

---
+++ GinServer/README.md
@@ -0,0 +1,7 @@
+# Gin
+
+Gin を試してみる．
+
+https://github.com/gin-gonic/gin
+
+
--- GinServer/app.go
+++ GinServer/app.go
@@ -2,23 +2,47 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func main() {
-	router := gin.Default()
-	router.LoadHTMLGlob("../templates/*.html")
+const TEMPLATE_DIR = "./templates/"
 
-	router.GET("/", func(ctx *gin.Context) {
+func main() {
+	r := gin.Default()
+
+	// Quick Stark
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	// Response Web Page
+	r.LoadHTMLGlob(TEMPLATE_DIR + "*.html")
+	r.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(200, "index.html", gin.H{})
 	})
-
-	router.GET("/xss", func(ctx *gin.Context) {
+	r.GET("/xss", func(ctx *gin.Context) {
 		ctx.HTML(200, "xss.html", gin.H{})
 	})
-	router.POST("/xss", func(ctx *gin.Context) {
+	r.POST("/xss", func(ctx *gin.Context) {
 		data := ctx.PostForm("data")
 		// そのまま表示してもデフォルトでHTMLタグはサニタイズされた
 		ctx.HTML(200, "xss.html", gin.H{"data": data})
 	})
-	router.Run()
+
+	//parameter
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+
+	//default port: 8000
+	r.Run(":8000")
 }
